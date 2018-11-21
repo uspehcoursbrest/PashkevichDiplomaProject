8b003ee7536b89975a4225d8d8389564a3f0efa5
Vadimpash <pash.vadim.99@bk.ru>
2018-11-21 09:02:14 +0300

    jQuery

---
+++ JS/JS/script.js
@@ -0,0 +1,47 @@
+window.onload = function () {
+    function clock() {
+        var date = new Date(),
+            day = date.getDate(),
+            month = date.getMonth(),
+            monthArr = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"],
+        year = date.getFullYear(),
+            hour = date.getHours(),
+            min = date.getUTCMinutes();
+
+        if(day < 10) day = "0" +day;
+        if(hour < 10) hour = "0" +hour;
+        if(min < 10) min = "0" +min;
+
+        document.getElementById("date").innerHTML = day + "" + monthArr[month] + "" + year;
+        document.getElementById("time").innerHTML = hour + ":" + min;
+
+    }
+    var timer;
+    function clockStart() {
+        timer = setInterval(clock, 60000);
+        clock();
+    }
+    clockStart();
+}
+
+$(document).ready(function () {
+
+    var btnUp = $(".up");
+    btnUp.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1000) {
+            btnUp.fadeIn();
+        } else {
+            btnUp.fadeOut();
+
+        }
+
+    })
+    btnUp.click(function () {
+        $("body,html").animate({
+            scrollTop: 0
+        }, 1000);
+        return false;
+    })
+})
+
